96609ca9e1da01b98ce1240a32762314c66c81a4
ubbelolhf@gmail.com <ubbelolhf@gmail.com@24e2a894-e9e0-c24e-43e9-9654d35d2dc6>
2012-04-20 20:08:38 +0000

    Added CallStatement and basic support for Loops (not finished yet)

---
+++ trunk/FastEmit/FastEmit/Core/For.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace FastEmit.Core
+{
+    public class ForLoop : Statement
+    {
+        private readonly Expression _condition;
+        private Scope _loopBlock;
+
+        public ForLoop(Method method, Expression condition) : base(method)
+        {
+            _condition = condition;
+        }
+
+        public ForLoop Do(Action<Scope> scope)
+        {
+            _loopBlock = new Scope();
+            var oldScope = Method.ActiveScope;
+
+            Method.ActiveScope = _loopBlock;
+
+            scope(_loopBlock);
+
+            Method.ActiveScope = oldScope;
+
+            return this;
+        }
+
+        public override void Emit(EmitContext context)
+        {
+            if(_loopBlock == null)
+                throw new InvalidOperationException("Loop block must be set");
+
+            var loopStart = context.Generator.DefineLabel();
+            context.Generator.MarkLabel(loopStart);
+
+            _loopBlock.Emit(context); // Inside loop
+            _condition.Emit(context); // Loop condition
+
+            context.Generator.Emit(OpCodes.Brtrue, loopStart);
+        }
+
+    }
+
+    public static class ForExtensions
+    {
+        public static ForLoop For(this Scope scope, System.Linq.Expressions.Expression<Func<bool>> condition)
+        {
+            var builder = new ExpressionBuilder<bool>(scope.Method, condition);
+            var statement = new ForLoop(scope.Method, builder.Expression);
+            scope.Children.Add(statement);
+
+            return statement;
+        }
+    }
+}
